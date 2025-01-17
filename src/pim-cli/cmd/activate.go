ee17db848d60d5b798b86ff2a011efea56c8221a
Anders <spaelling@gmail.com>
2025-01-17 21:03:22 +0100

    Enhance role activation command: Add validation flag and improve error handling for Entra ID role activation

--- src/pim-cli/cmd/activate.go
+++ src/pim-cli/cmd/activate.go
@@ -18,8 +18,10 @@ func init() {
 	activateEntraEligibleCmd.Flags().StringP("justification", "j", "", "justification for activating the role")
 	activateEntraEligibleCmd.MarkFlagRequired("justification")
 	activateEntraEligibleCmd.Flags().IntP("duration", "d", 240, "duration for the role activation in minutes")
+	activateEntraEligibleCmd.Flags().BoolP("validation", "z", false, "validate the role activation request")
 }
 
+// MARK: activate
 var activateCmd = &cobra.Command{
 	Use:   "activate",
 	Short: "",
@@ -27,71 +29,106 @@ var activateCmd = &cobra.Command{
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
 
+// MARK: activate az
 var activateAzureCmd = &cobra.Command{
 	Use:   "az",
-	Short: "activate azure resources",
+	Short: "",
 	Long:  ``,
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: print help
-		fmt.Printf("Usage: pim-cli activate az [eligible]\n")
-	},
+	Run:   func(cmd *cobra.Command, args []string) {},
 }
 
+// MARK: activate entra
 var activateEntraCmd = &cobra.Command{
 	Use:   "entra",
-	Short: "activate Entra ID roles",
+	Short: "",
 	Long:  ``,
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: print help
-		fmt.Printf("Usage: pim-cli activate entra [eligible]\n")
-	},
+	Run:   func(cmd *cobra.Command, args []string) {},
 }
 
+// MARK: activate az eligible
 var activateAzureEligibleCmd = &cobra.Command{
 	Use:   "eligible",
 	Short: "Activate eligibility for azure resources",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		// tenantId := util.GetTenantId()
-		// accessToken := util.GetAccessToken(tenantId, util.AZ_MGMT_SCOPE)
-		// fmt.Printf("token: %s\n", accessToken)
-
+		fmt.Printf("Not implemented yet\n")
 	},
 }
 
+// MARK: activate entra roles
 var activateEntraEligibleCmd = &cobra.Command{
 	Use:   "eligible",
 	Short: "Activate eligible Entra ID role",
 	Long: `
-pim activate entra eligible --role "58a13ea3-c632-46ae-9ee0-9c0d43cd7f3d" `,
+usage example:
+
+validate role activation:
+
+pim activate entra eligible --role "58a13ea3-c632-46ae-9ee0-9c0d43cd7f3d" --justification "Enable admin role using pim cli" --duration 42 --validation true
+
+Role activation is validated!
+Action:                 selfActivate
+Principal Id:           65cfef21-f882-40a8-acc4-e00eeb156088
+RoleDefinition Id:      58a13ea3-c632-46ae-9ee0-9c0d43cd7f3d
+Is validation:          true
+Justification:          Enable admin role using pim cli
+Activation start:       2025-01-17T19:59:30Z
+Expires after:          PT42M
+`,
 
 	Run: func(cmd *cobra.Command, args []string) {
+		// get roleid, justification, and duration from flags
+		// return if we encounter an error
 		roleid, err := cmd.Flags().GetString("role")
 		if err != nil {
-			fmt.Printf("Error: %s\n", err)
+			fmt.Printf("Error getting role id: %s\n", err)
+			return
 		}
 		justification, err := cmd.Flags().GetString("justification")
 		if err != nil {
-			fmt.Printf("Error: %s\n", err)
+			fmt.Printf("Error getting justification: %s\n", err)
+			return
 		}
-		//"PT4H"
 		durationMinutes, err := cmd.Flags().GetInt("duration")
 		if err != nil {
-			fmt.Printf("Error: %s\n", err)
+			fmt.Printf("Error getting duration: %s\n", err)
+			return
 		}
+		if durationMinutes > 8*60 {
+			fmt.Printf("Error: duration must be less than 8 hours/480 minutes\n")
+			return
+		}
+		// convert duration to PT{hours}H{minutes}M format
 		duration := fmt.Sprintf("PT%dH%dM", durationMinutes/60, durationMinutes%60)
-		roleActivationRequestResponse, err := util.ActivateEntraIdEligibleRoles(roleid, justification, duration)
+		// IsValidationOnly
+		isValidation, err := cmd.Flags().GetBool("validation")
 		if err != nil {
-			fmt.Printf("Error: %s\n", err)
+			fmt.Printf("Error getting validationflag: %s\n", err)
+			return
 		}
-		// TODO
-		fmt.Printf("Action: %s\n", roleActivationRequestResponse.Action)
-		fmt.Printf("PrincipalId: %s\n", roleActivationRequestResponse.PrincipalId)
-		fmt.Printf("RoleDefinitionId: %s\n", roleActivationRequestResponse.RoleDefinitionId)
-		fmt.Printf("DirectoryScopeId: %s\n", roleActivationRequestResponse.DirectoryScopeId)
-		fmt.Printf("IsValidationOnly: %t\n", roleActivationRequestResponse.IsValidationOnly)
-		fmt.Printf("Justification: %s\n", roleActivationRequestResponse.Justification)
-		fmt.Printf("ScheduleInfo: StartDateTime=%s, Expiration=%v\n", roleActivationRequestResponse.ScheduleInfo.StartDateTime, roleActivationRequestResponse.ScheduleInfo.Expiration)
+
+		// activate the role
+		roleActivationRequestResponse, err := util.ActivateEntraIdEligibleRoles(roleid, justification, duration, isValidation)
+		if err != nil {
+			fmt.Printf("Error when activating role: %s\n", err)
+		}
+
+		if isValidation {
+			fmt.Print("Role activation is validated!\n")
+		} else {
+			fmt.Print("Role activation is succesfull!\n")
+		}
+
+		// TODO: look up name of the role
+
+		// print the response
+		fmt.Printf("Action:\t\t\t%s\n", roleActivationRequestResponse.Action)
+		fmt.Printf("Principal Id:\t\t%s\n", roleActivationRequestResponse.PrincipalId)
+		fmt.Printf("RoleDefinition Id:\t%s\n", roleActivationRequestResponse.RoleDefinitionId)
+		fmt.Printf("Is validation:\t\t%t\n", roleActivationRequestResponse.IsValidationOnly)
+		fmt.Printf("Justification:\t\t%s\n", roleActivationRequestResponse.Justification)
+		fmt.Printf("Activation start:\t%s\n", roleActivationRequestResponse.ScheduleInfo.StartDateTime)
+		fmt.Printf("Expires after:\t\t%v\n", roleActivationRequestResponse.ScheduleInfo.Expiration.Duration)
 
 	},
 }
